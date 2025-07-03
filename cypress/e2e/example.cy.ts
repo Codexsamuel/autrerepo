78c59eee7941a9b397a6b277b38e238fa0ba8122
Sabine Lucie Nga <sabinelucie@MacBook-Pro-de-Sabine.local>
2025-07-03 23:10:58 +0200

    Netlify: clean dev deps, remove cypress/prisma files, fix build

--- cypress/e2e/example.cy.ts
+++
@@ -1,6 +0,0 @@
-describe('Example E2E Test', () => {
-  it('should visit the homepage', () => {
-    cy.visit('/');
-    cy.get('body').should('exist');
-  });
-}); 
--- cypress/support/e2e.ts
+++
@@ -1,4 +0,0 @@
-// Support file pour Cypress E2E
-// Ajoutez ici vos commandes personnalisées si besoin
-
-export {} 
